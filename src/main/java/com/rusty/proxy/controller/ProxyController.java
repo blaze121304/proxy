bdc8846b263211b88ab8ca9f7a293935aa0f075d
rusty <blaze121304@gmail.com>
2024-05-22 10:25:25 +0900

    [2024-05-17] Proxy Server - controller 및 service 구현

--- src/main/java/com/rusty/proxy/controller/ProxyController.java
+++ src/main/java/com/rusty/proxy/controller/ProxyController.java
@@ -1,12 +1,10 @@
 package com.rusty.proxy.controller;
 
 import com.rusty.proxy.domain.dto.TaxBill;
+import com.rusty.proxy.domain.dto.User;
 import com.rusty.proxy.domain.service.ProxySerivce;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/proxy")
@@ -19,14 +17,30 @@ public class ProxyController {
         this.proxySerivce = proxySerivce;
     }
 
-    @GetMapping("/tax/bill")
-    private TaxBill getPaymentBill(TaxBill taxBill){
-
-        return proxySerivce.getPaymentBill(taxBill);
+    @GetMapping("/user/{id}")
+    private User findUserById(@PathVariable String id){
+        return proxySerivce.find(id);
     }
 
+    @GetMapping("/user")
+    private TaxBill findAll(TaxBill taxBill){
+        return null;
+    }
 
+    @GetMapping("/tax/{id}")
+    private TaxBill findTaxById(TaxBill taxBill){
+        return null;
+    }
 
+    @PostMapping("/reg/{id}")
+    private TaxBill registId(TaxBill taxBill){
+        return null;
+    }
+
+    @PostMapping("/modify/{id}")
+    private TaxBill modifyId(TaxBill taxBill){
+        return null;
+    }
 
 
 }
--- src/main/java/com/rusty/proxy/domain/service/ProxyServiceImpl.java
+++ src/main/java/com/rusty/proxy/domain/service/ProxyServiceImpl.java
@@ -1,17 +1,34 @@
 package com.rusty.proxy.domain.service;
 
 import com.rusty.proxy.domain.dto.TaxBill;
+import com.rusty.proxy.domain.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ProxyServiceImpl implements ProxySerivce{
 
-    /**
-     * @return bill
-     */
-    @Override
-    public TaxBill getPaymentBill(TaxBill taxBill) {
-        return taxBill;
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public ProxyServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
+    /**
+     * @param id
+     * @return
+     */
+    @Override
+    public User find(String id) {
+
+        User user = new User();
+        user.setId("rusty");
+        user.setAddr("부산");
+        user.setName("KIMKIWOOK");
+        user.setTaxNum("1124211");
+
+        return user;
+    }
 }
