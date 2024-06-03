baff4b0dca0ea93b356a6eea791b5de58ab68d13
rusty <blaze121304@gmail.com>
2024-06-03 17:58:40 +0900

    [2024-05-17] Proxy Server - controller 및 service 구현

---
+++ src/main/java/com/rusty/proxy/domain/dto/ProxyDto.java
@@ -0,0 +1,10 @@
+package com.rusty.proxy.domain.dto;
+
+public class ProxyDto {
+    String orgCode;
+    String userName;
+    String userAddress;
+    String carNo;
+    String violationCode;
+    String violationName;
+}
