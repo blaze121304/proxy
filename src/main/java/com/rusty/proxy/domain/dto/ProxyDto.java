fb1c64348218c280a28d4f9c6f5b7aa338bd1e0c
rusty <blaze121304@gmail.com>
2024-06-04 11:03:54 +0900

    [2024-05-17] Proxy Server - controller 및 service 구현

--- src/main/java/com/rusty/proxy/domain/dto/ProxyDto.java
+++ src/main/java/com/rusty/proxy/domain/dto/ProxyDto.java
@@ -1,5 +1,12 @@
 package com.rusty.proxy.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProxyDto {
     String orgCode;
     String userName;
@@ -7,4 +14,23 @@ public class ProxyDto {
     String carNo;
     String violationCode;
     String violationName;
+    String violationDivision;
+    String violationDetail;
+    String violationDt;
+    String violationLocation;
+    String volunteerPaymentPeriod;
+    String mainTax;
+    String surcharge;
+    String addSurcharge;
+    String violationFee;
+    String confirmDate;
+    String dueDate;
+    String virtualBank;
+    String virtualBankPaymentNo;
+    String electronicPaymentNo;
+    String violationImage01;
+    String violationImage02;
+    String violationImage03;
+    String violationImage04;
+
 }
