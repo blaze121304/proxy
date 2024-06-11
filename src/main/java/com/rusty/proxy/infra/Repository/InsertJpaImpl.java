d1f30caa828f473fcf7a8e76429e990ffef3ba3c
rusty <blaze121304@gmail.com>
2024-06-11 15:54:56 +0900

    [2024-05-17] Proxy Server - controller 및 service 구현

--- src/main/java/com/rusty/proxy/infra/Repository/InsertJpaImpl.java
+++ src/main/java/com/rusty/proxy/infra/Repository/InsertJpaImpl.java
@@ -1,7 +1,28 @@
 package com.rusty.proxy.infra.Repository;
 
+import com.rusty.proxy.domain.dto.CarDto;
+import com.rusty.proxy.domain.dto.ProxyDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class InsertJpaImpl implements InsertJpa {
+    /**
+     * @param requestEntity
+     * @param responseEntity
+     */
+    @Override
+    public void excute(HttpEntity<CarDto> requestEntity, ResponseEntity<ProxyDto> responseEntity) {
+
+    }
+
+    /**
+     * @param responseEntity
+     * @param requestEntity
+     */
+    @Override
+    public void excute(ResponseEntity<ProxyDto> responseEntity, HttpEntity<String> requestEntity) {
+
+    }
 }
